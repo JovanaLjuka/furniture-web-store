04f61dbcfd6df2ed7d8b2a95d8e0b3cab38a6c85
Jovana Ljuka <jovanaljuka1@gmail.com>
2024-04-02 14:31:59 +0200

    cors

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ const port = process.env.PORT || 5002
 
 // middleware
 app.use(express.json())
-// app.use(cors())
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+
+app.use(cors())
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 // console.log(path.join(__dirname, 'public'))
